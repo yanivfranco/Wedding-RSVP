afa77f3baca946dbcdc3dcfc6f2e2e4655161661
yanivfranco <yanivfranco@gmail.com>
2017-08-27 13:24:54 +0300

    almost done

---
+++ index.php
@@ -0,0 +1,187 @@
+
+<?php
+	//debug
+	ini_set('display_errors', 'On');
+	error_reporting(E_ALL | E_STRICT);
+	session_start();
+	//sms gateway login
+	include "smsGateway.php";
+	$smsGateway = new SmsGateway('yanivfranco@gmail.com', 'smsgatewayfa1g50');
+	$device_id = 57323;
+	//functions
+	function sqlNoResult($query){
+		$conn = new mysqli($_SESSION["host"],$_SESSION["username"],$_SESSION["password"],$_SESSION["db_name"]);
+		if ($conn->query($query) === TRUE) {
+			    return 1;
+		} else {
+			   return $conn->error;
+		}
+	}
+	function sqlResult($query){
+		$conn = new mysqli($_SESSION["host"],$_SESSION["username"],$_SESSION["password"],$_SESSION["db_name"]);
+		return mysqli_query($conn, $query);
+
+	}
+	function generateRandomString($length = 8) {
+	    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	    $charactersLength = strlen($characters);
+	    $randomString = '';
+	    for ($i = 0; $i < $length; $i++) {
+	        $randomString .= $characters[rand(0, $charactersLength - 1)];
+	    }
+	    return $randomString;
+	}
+?>
+<html>
+<head>
+<title>Wedding SMS</title>
+<link href="style.css" rel='stylesheet' type='text/css' />
+<link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>	
+</head>
+<body>
+<!-- MySQL connection and setup -->
+<div class="db_connect">
+	<h>MySQL Setup</h><br>
+	<p><?php if(isset($_SESSION["host"])) echo "There are saved connection details: ".$_SESSION["host"]." ".$_SESSION["username"]." ".$_SESSION["password"]." ".$_SESSION["db_name"]?></p>
+	<form action="" method="post" enctype="multipart/form-data">
+		  MySQL host: <input type="text" name="host" id="host"/><br>
+		  MySQL username: <input type="text" name="username" id="username"/><br>
+		  MySQL password: <input type="text" name="password" id="password"/><br>
+		  MySQL database name: <input type="text" name="db_name" id="db_name"/><br>
+		  <input type="submit" name="db_connect" value="Store connection details"/>
+	</form>
+	<?php
+		if(isset($_POST['db_connect'])){ //check if form was submitted
+			$_SESSION["host"] = $_POST['host'];
+			$_SESSION["username"] = $_POST['username'];
+			$_SESSION["password"] = $_POST['password'];
+			$_SESSION["db_name"] = $_POST['db_name'];
+			echo "Details stored";
+		}
+
+	?>
+	<!-- create database after connecting -->
+	<form action="" method="post" enctype="multipart/form-data">
+			Create new database:  <input type="text" name="db_name" id="db_name"/><br>
+		  <input type="submit" name="create_db" value="Create database"/>
+	</form>
+	<?php
+		if(isset($_POST['create_db'])){
+			$db_name = $_POST['db_name'];
+			$query = "CREATE DATABASE " . $db_name;
+			$result = sqlNoResult($query);
+			if($result == TRUE) echo "Create " .$db_name .  " database succeded";
+			else echo "Create table failed: " . $conn->error;
+		}
+	?>
+	<!-- drop table -->
+		<form action="" method="post" enctype="multipart/form-data">
+			Drop Table:  <input type="text" name="drop_name" id="drop_name"/><br>
+		  <input type="submit" name="drop" value="Drop"/>
+	</form>
+	<?php
+		if(isset($_POST['drop'])){
+			$drop_name = $_POST['drop_name'];
+			$query = "DROP TABLE $drop_name;";
+			$result = sqlNoResult($query);
+			if($result == TRUE) echo "<span style='font-size: 70%; color: red; margin: 0;'>Drop table " .$drop_name .  " succeded</span>";
+			else echo "Drop table failed: " . $conn->error;
+		}
+	?>
+</div>
+
+
+
+
+<!-- exel file handeling -->
+<div class="xlupload">
+	<h>CSV Guests file upload to database </h>
+	table will be [id,name,phone,amount,token,is_confirmed(1/0), actual_amount]
+	<form action="" method="post" enctype="multipart/form-data">
+	  Select exel guests file: <input type="file" name="file" id="file"/><br>
+	  Choose SQL table name: <input type="text" name="table_name" id="table_name"/><br>
+	  <input type="submit" name="file_submit" value="Enter To Database"/>
+	</form>
+
+	<!-- php for exel handeling -->
+	<?php
+		if(isset($_POST['file_submit'])){ //check if form was submitted
+			if(ctype_alnum($_POST['table_name'])){ //checks if table name is valid
+				$table_name = $_POST['table_name'];
+			}
+			else{
+				echo "Table name is not valid";
+				return;
+			}
+			$filename = $_FILES['file']['name'];
+			$allowed =  array('csv');
+			$ext = pathinfo($filename, PATHINFO_EXTENSION);
+			if(!in_array($ext,$allowed) ) {//checks if file is csv
+			    echo 'error - not a CSV file!';
+			}
+			else{//if its a valid csv file
+				//creates a table in the database
+				$query = "CREATE TABLE $table_name (
+				id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+				name VARCHAR(30) NOT NULL,
+				phone VARCHAR(50),
+				amount INT(2),
+				token VARCHAR(10),
+				is_confirmed INT(1),
+				actual_amount INT(2)
+				)";
+				$result = sqlNoResult($query);
+				if($result == 1) echo "Table $table_name created successfuly<br>";
+				else echo "Table $table_name create failed $result<br>";
+
+				//inserting the csv information into database
+				$filepath = $_FILES["file"]["tmp_name"];
+				$file = fopen($filepath,"r");
+				$line = fgetcsv($file);
+				 while(! feof($file)){
+				 	$id = $line[0]; $name = $line[1]; $phone = $line[2]; $amount = $line[3]; $token = generateRandomString();
+				 	$query = "INSERT INTO $table_name (id, name, phone, amount,token,actual_amount)
+					VALUES ($id,'$name',$phone,$amount,'$token',$amount)";
+					$result = sqlNoResult($query);
+					if($result == 1) echo "line insert to $table_name succeded<br>";
+					else echo "line insert to $table_name failed: $result<br>";		
+					$line = fgetcsv($file);		  }
+			}
+		}
+
+	?>
+</div>
+
+<div>
+	<h>SMS to guests</h>
+	<form action="" method="post" enctype="multipart/form-data">
+		Index page url: <input type="text" name="index_page"><br>
+		Table name in database: <input type="text" name="table_name"><br>
+		Messege before the link and after "hello name,": <input type="text" name="message" style="width: 250px; height: 150px"><br>
+		<input type="submit" name="sms_submit" value="Generate links & Send SMS"/>
+	</form>
+
+<!-- php for sms sending -->
+	<?php
+		if(isset($_POST['sms_submit'])){ //check if form was submitted
+			$index_page = $_POST['index_page'];
+			$message = $_POST['message'];
+			$table_name = $_POST['table_name'];
+			$result = sqlResult("SELECT id, name, phone, token FROM $table_name");
+			//iterate over the guests, building links and personal messages and sends sms.
+			if(mysqli_num_rows($result) > 0){
+				while($row = mysqli_fetch_array($result)){
+					$id = $row["id"]; $name = $row["name"]; $phone = $row["phone"]; $token = $row["token"]; //getting guest info
+					$link="$index_page/?id=$id&token=$token";
+					$complete_message = "helli $name,\n$message, \n$link";
+					//sends sms
+					$smsGateway->sendMessageToNumber($phone, $complete_message, $device_id);
+
+				}
+			}
+		}
+	?>
+
+</div>
+</body>
+</html>
---
+++ ofir-yael/index.php
@@ -0,0 +1,122 @@
+<?php 
+	//debug
+	ini_set('display_errors', 'On');
+	error_reporting(E_ALL | E_STRICT);
+	session_start();
+	//functions
+	function sqlResult($query){
+		$conn = new mysqli($_SESSION["host"],$_SESSION["username"],$_SESSION["password"],$_SESSION["db_name"]);
+		return mysqli_query($conn, $query);
+
+	}
+	if ((isset($_GET['id'])) && (isset($_GET['token']))){
+		$id = $_GET['id']; $token = $_GET['token'];
+		$_SESSION['id'] = $_GET['id'];
+		$_SESSION['token'] = $_GET['token'];
+		$query = "SELECT token, name, actual_amount FROM ofiryael WHERE id=$id;";
+		$result = sqlResult($query);
+		//checking tokens
+		if(mysqli_num_rows($result) > 0){
+			$row = mysqli_fetch_array($result);
+			$db_token = $row['token']; $actual_amount = $row['actual_amount'] ; $name = $row['name'];
+			if($token != $db_token) { //if token not the same
+				header('HTTP/1.0 403 Forbidden');
+				echo 'Forbidden!';
+				exit;
+			}
+			
+		}
+		else{//id not found
+			header('HTTP/1.0 403 Forbidden');
+			echo 'Forbidden!';
+			exit;
+		}
+	}
+	else{//if no parameters token and id
+		header('HTTP/1.0 403 Forbidden');
+		echo 'Forbidden!';
+		exit;
+	}
+
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Ofir & Yael</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />	
+<link href="css/jquery.bootstrap-touchspin.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/jquery-1.11.1.min.js"></script>
+<script src="js/jquery.bootstrap-touchspin.js"></script>
+<!--webfonts-->
+  <link href='http://fonts.googleapis.com/css?family=Lora:400,400italic|Niconne' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/earlyaccess/opensanshebrewcondensed.css' rel='stylesheet' type='text/css'>
+<!--//webfonts-->
+</head>
+<body>
+
+<div class="banner">
+	<div class="container">
+		<li>
+			<div class="banner-info">
+				<h3>Ofir <lable>&</lable> Yael</h3>
+				<p>24/10/2017</p>
+			</div>
+		</li>
+	</div>
+</div>
+<!--//banner-->
+	<!--/welcome-->
+	<div class="welcome-section">
+		<div class="container">
+		   <h2>שלום <?php echo "$name" ;?></h2>
+		   <p>אופיר ויעל מתרגשים להזמינכם לטקס הנישואים שיתקיים ביום שבת ה</p>
+		   <p2> 19:30 24/10/2017 </p2><br>
+		   <p1> באקו גן אירועים - חדרה</p1><br><br>
+		   <p2>:כמות האורחים שמגיעים</p2>
+			<div class="input-group input-group-lg" style="direction: ltr;font-weight:bold;font-size:23px;width: 150px; margin: 5px auto;">
+				<form action="" method="post">
+				<input id="numOfGuests" type="text" value="<?php echo "$actual_amount"; ?>" name="numOfGuests" class="form-control input-lg" style="background-color: #ebe3de; text-align: center;" 		data-bts-step-interval-delay="500",
+						data-bts-min="0"
+				       data-bts-max="20"
+					    data-bts-booster="false"
+						data-bts-mousewheel="false"
+					   data-bts-step-interval-delay="500"
+					   data-bts-init-val="2"
+					   >
+			</div>
+			<script>
+			    $("input[name='numOfGuests']").TouchSpin({
+			        postfix: "",
+			        postfix_extraclass: "",
+			    });
+			</script>
+			
+             <input type="submit" name="no" class="button-form" id="no" style="background: transparent;border: none;background-image: url(images/x-mark.png);background-size: 100%; height: 50px; width: 50px; margin-right: 50px" value='' />
+             <input type="submit" name="yes" class="button-form" id="yes" style="background: transparent;border: none;background-image: url(images/check-mark.png);background-size: 100%; height: 50px; width: 50px ;margin-left: 50px" value='' /><br>
+             <span class="attend" style="margin-left: 50px">מגיע/ה</span><span class="attend" style="margin-right: 50px">לא מגיע/ה</span>
+			</form>
+	</div>
+</body>
+</html>
+
+<?php
+	if(isset($_POST['no'])){
+		$query = "UPDATE ofiryael
+				SET is_confirmed = 0
+				WHERE id = $id;";
+		$result = sqlResult($query);
+	}
+	if(isset($_POST['yes'])){
+		$actual_amount = $_POST['numOfGuests'];
+		$query = "UPDATE ofiryael
+				SET is_confirmed = 1, actual_amount= $actual_amount
+				WHERE id = $id;";
+		$result = sqlResult($query);
+	}
+
+
+?>
