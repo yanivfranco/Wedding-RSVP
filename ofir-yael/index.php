485b51f3697d08d7bc70401ec9535e6e96c0f3e8
yaniv <yanivfranco@gmail.com>
2017-10-15 16:24:49 +0300

    add sms status update

--- ofir-yael/index.php
+++ ofir-yael/index.php
@@ -252,8 +252,7 @@
 											WHERE id = $id;";
 									$tempres = sqlNoResult($query);
 									//checking sql results and counting
-									if($tempres == 1) echo "insert success";
-									else {
+									if($tempres == 1){
 										$error = $tempres;
 										echo $error;
 									}
